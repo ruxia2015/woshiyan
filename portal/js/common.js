b18c189e972e4f9534c21b45c944e85ec121d998
ruxia2015 <ruxia2010@126.com>
2019-10-29 15:23:16 +0800

    音标文件

--- portal/js/common.js
+++ portal/js/common.js
@@ -1,8 +1,25 @@
 function pinyinRead(pinyin) {
 
+
+
+
+    //var filePath ='https://hanyu-word-pinyin-short.cdn.bcebos.com/' + pinyin + '.mp3';
+     var filePath ='http://appcdn.fanyi.baidu.com/zhdict/mp3/' + pinyin + '.mp3';
+    audioPlay(filePath);
+}
+
+
+
+function soundPlay(path) {
+    var filePath ="./audio/"+path;
+    audioPlay(filePath);
+}
+
+
+
+function audioPlay(filePath){
     var au1 = '<audio id="tts_autio_id" autoplay="autoplay">';
-    var sss = '<source id="tts_source_id" src="https://hanyu-word-pinyin-short.cdn.bcebos.com/' + pinyin + '.mp3">';
-    //var sss = '<source id="tts_source_id" src="http://appcdn.fanyi.baidu.com/zhdict/mp3/' + pinyin + '.mp3">';
+    var sss = '<source id="tts_source_id" src="'+filePath+'">';
     var eee = '<embed id="tts_embed_id" height="0" width="0" src="">';
     var au2 = '</audio>';
 
