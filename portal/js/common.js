3912480dda772bd7479c4743bfb9393ce458143d
ruxia2015 <ruxia2010@126.com>
2019-11-05 16:30:38 +0800

    英语读音

--- portal/js/common.js
+++ portal/js/common.js
@@ -1,11 +1,45 @@
+String.prototype.format = function () {
+    var values = arguments;
+    return this.replace(/\{(\d+)\}/g, function (match, index) {
+        if (values.length > index) {
+            return values[index];
+        } else {
+            return "";
+        }
+    });
+};
+
+
 function pinyinRead(pinyin) {
-
-
-
-
     var filePath ='https://hanyu-word-pinyin-short.cdn.bcebos.com/' + pinyin + '.mp3';
     // var filePath ='http://appcdn.fanyi.baidu.com/zhdict/mp3/' + pinyin + '.mp3';
     audioPlay(filePath);
+}
+
+/**
+ *
+ * @param word
+ * @param way  1 有道   2 百度
+ */
+function englishRead(word,way,speed){
+
+    var wayUrl = {"yd":"http://tts.youdao.com/fanyivoice?word={0}&le=eng",
+    "bd":"https://fanyi.baidu.com/gettts?lan=en&text={0}&spd={1}&source=web"}
+
+    if(!way){
+        way = "bd";
+    }
+
+    if(!speed){
+        speed = 2;
+    }
+    var url = wayUrl[way];
+    url = url.format(word,speed);
+
+    // url = url.format({word:word});
+
+
+    audioPlay(url);
 }
 
 
@@ -32,3 +66,38 @@ function audioPlay(filePath){
     ttsAudio = document.getElementById('tts_autio_id');
     ttsAudio.play();
 }
+
+
+function moduleLoad(url) {
+    $.get(url, function (result) {
+        var html = $(result);
+        var __templates = html;
+        $("[slot]").each(function () {
+            var id = $(this).attr('slot');
+            var body = $(__templates).find('#' + id).html();
+            $(this).html(body);
+        });
+    });
+}
+
+// function autoCreateCompent(url){
+
+//  $.get(url, function (result) {
+//         var html = $(result);
+//         var __templates = html;
+
+//         $(__templates).find("templates>template").each(function(){
+//             var $this = $(this);
+//             var id =$this.attr("id");
+//             var body = $this.html();
+//             Vue.component("hello-component",{
+//                 props:["message"],
+//                 template :"<div ><h1>组件定义之全局组件</h1><h4>{{message}}</h4></div>"
+//             });
+//         })
+
+//     });
+
+// }
+
+// autoCreateCompent("../template/page-template.html");
